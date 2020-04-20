60e3d316ca3f86d6254e3b88627bddc38e39a925
RomainGrx <romain.graux@student.uclouvain.be>
2020-04-20 12:28:50 +0200

    Update all Q Learning scripts

--- src/Q-Learning/QL-MountainCar.py
+++ src/Q-Learning/QL-MountainCar.py
@@ -1,72 +1,136 @@
-#!/usr/bin/env python3
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 """
-Created on Tue Aug 13 11:16:55 2019
-
-@author: romaingraux
+@author : Romain Graux
+@date : Friday, 17 April 2020
 """
 
-
-
-# import tflearn as tf
-import numpy as np
+import os
 import gym
+import random
+import numpy as np
+import tensorflow as tf
 import matplotlib.pyplot as plt
 
-def adjust(state):
-    return np.round(state * np.array([10, 100])).astype(int)
 
+# Global variables
 env = gym.make('MountainCar-v0')
-episodes = 1000
-LR = 0.2
-gamma = 0.9
-min_eps = 0
-epsilon = 0.5
+
+discount_factor = 0.95 # discount factor
+render_frequency = 1000
+epochs = 25_000
+
+discrete_observation_shape = (25, 25)
+
+epsilon_high = 0.95
+epsilon_low = 0.1
+epsilon_decay = epochs//4
+
+learning_rate_high = 0.3
+learning_rate_low = 0.05
+learning_rate_decay = epochs//10
+
+# Training
+
+def get_finite_state(state):
+    discrete_state = ((state - env.observation_space.low)/ (env.observation_space.high - env.observation_space.low))* discrete_observation_shape
+    return tuple(discrete_state.astype(int))
+
+if not os.path.exists('save_numpy'):
+    os.makedirs('save_numpy')
+
+NAME = f'QL-MountainCar_{discrete_observation_shape}_LRMIN={learning_rate_low}_LRMAX={learning_rate_high}_LRDECAY={learning_rate_decay}_EPMIN={epsilon_low}_EPMAX={epsilon_high}_EPDECAY={epsilon_decay}'
+WRITER = tf.summary.create_file_writer(f'tensorboard/{NAME}')
+
+epsilon = epsilon_high
+learning_rate = learning_rate_high
+
+qtable = np.random.uniform(low=-2, high=0, size=(discrete_observation_shape + (env.action_space.n,)))
+# qtable = np.load('./save_numpy/baseline_without_epsilon_(20, 20, 3).npy') ; epsilon = 0; epsilon_low=0; epsilon_high=0
+
 reward_list = []
-ave_reward_list = []
-max_reward_list = []
-reduction = epsilon / episodes
+loss_list = []
 
-try:
-    states = (env.observation_space.high - env.observation_space.low) * np.array([10, 100])
-    states = np.round(states).astype(int) + 1
-    Q = np.zeros((states[0], states[1], env.action_space.n))
-    for e in range(episodes):
-            rewardTot, reward = 0, 0
-            state = env.reset()
-            state = adjust(state)
-            done = False
-            while done != True:
-                if (e+1) % 500 == 0 : env.render()
-                if np.random.random() < 1 - epsilon :
-                    action = np.argmax(Q[state[0], state[1]])
-                else :
-                    action = np.random.randint(0, env.action_space.n)
-                state2, reward, done, info = env.step(action)
-                state2 = adjust(state2)
-                delta = LR*(reward + gamma*np.max(Q[state2[0], state2[1]]) - Q[state[0], state[1],action])
-                Q[state[0], state[1], action] += delta
-                state = state2
-                rewardTot += reward
-                epsilon -= reduction
-            reward_list.append(rewardTot)
-            if (e+1) % 100 == 0:
-                print('Episode {} Average Reward: {} Maximum Reward: {}'.format(e+1, np.mean(reward_list), np.max(reward_list)))
-                ave_reward_list.append(np.mean(reward_list))
-                max_reward_list.append(np.max(reward_list))
-                reward_list = []
+for episode in range(epochs):
+    done = False
+    reward_values = []
+    loss_values = []
+    finite_state = get_finite_state(env.reset())
+    step = 0
+    REACH = False
+    while not done:
+        if episode % render_frequency == 0: env.render()
+        if random.random() < epsilon:
+            action = random.randint(0, env.action_space.n-1)
+        else:
+            action = np.argmax(qtable[finite_state])
+        next_state, reward, done, info = env.step(action)
 
-    plt.plot(100*(np.arange(len(ave_reward_list)) + 1), ave_reward_list, 'y-')
-    plt.plot(100*(np.arange(len(ave_reward_list)) + 1), max_reward_list, 'r-')
-    plt.xlabel('Episodes')
-    plt.ylabel('Average Reward')
-    plt.title('Average Reward vs Episodes')
+        if next_state[0] > env.goal_position:
+            REACH = True
+            qtable[finite_state + (action,)] = 0
+            loss = 0
+        else:
+            next_finite_state = get_finite_state(next_state)
+            target_q = (reward + discount_factor * np.max(qtable[next_finite_state + (action,)]))
+            loss = abs(qtable[finite_state + (action,)] - target_q)
+            qtable[finite_state + (action,)] = (1 - learning_rate)*qtable[finite_state + (action,)] + learning_rate * target_q
+
+        loss_values += [loss]
+        reward_values += [reward]
+
+        step += 1
+        finite_state = next_finite_state
+
+    reward_list += [np.sum(reward_values)]
+    loss_list += [np.mean(loss_values)]
+
+    with WRITER.as_default():
+        tf.summary.scalar("loss", loss_list[-1], step=episode)
+        tf.summary.scalar("reward", reward_list[-1], step=episode)
+        tf.summary.scalar("reach_goal", int(REACH), step=episode)
+        tf.summary.scalar("learning_rate", learning_rate, step=episode)
+        tf.summary.scalar("epsilon", epsilon, step=episode)
+
+    print(f'episode {episode+1}/{epochs} : reward {reward_list[-1]} : loss {loss_list[-1]:.2f}: step {step} : epsilon {epsilon:.3f} : lr {learning_rate:.3f} : reach goal {REACH}')
+
+    epsilon = epsilon_low + (epsilon_high - epsilon_low)*np.exp(-episode/epsilon_decay)
+    learning_rate = learning_rate_low + (learning_rate_high - learning_rate_low)*np.exp(-episode/learning_rate_decay)
+
+np.save(f'save_numpy/{NAME}', qtable)
+env.close()
+
+def plot_array(array):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    m, n, l = array.shape
+    low = env.observation_space.low
+    high = env.observation_space.high
+    x,y,z = np.meshgrid(np.linspace(low[0], high[0], m), np.linspace(low[1], high[1], n), np.arange(l))
+    sca = ax.scatter(x,y,z, c=array)
+    ax.set_zticks([0,1,2]); ax.set_zticklabels(['Left', 'No push', 'Right'])
+    ax.set_xlabel('Position')
+    ax.set_ylabel('Velocity')
+    ax.set_zlabel('Action')
+    cbar=plt.colorbar(sca)
     plt.show()
 
-finally:
-    env.close()
+def plot_max_array(array):
+    fig = plt.figure()
+    plt.title('Representation by action')
+    m, n, l = array.shape
+    low = env.observation_space.low
+    high = env.observation_space.high
+    argmax = np.argmax(array, axis=2)
+    ret = plt.imshow(argmax)
+    plt.xticks(np.arange(m), list(map(lambda x:'%.2f'%x, np.linspace(low[0], high[0], m))))
+    plt.yticks(np.arange(n), list(map(lambda x:'%.2f'%x, np.linspace(low[1], high[1], n))))
+    plt.xlabel('Position')
+    plt.ylabel('Velocity')
+    cbar=plt.colorbar(ret)
+    cbar.set_ticks([0,1,2])
+    cbar.set_ticklabels(['Left', 'No push', 'Right'])
+    plt.show()
 
-
-
-
-
+plot_max_array(qtable)
+# plot_array(qtable)
